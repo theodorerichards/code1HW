a20c4c01dd33363a9eaeae2509f8795c363c04b8
Theodore Richards <richt075@newschool.edu>
2016-12-20 20:43:45 -0500

    added for final

--- code final/sketch.js
+++ code final/sketch.js
@@ -3,9 +3,14 @@
 // @DanielSchiffman https://www.youtube.com/watch?v=eGFJ8vugIWA&t=1039s
 // @BryanMa
 // @tfguy44 https://forum.processing.org/one/topic/random-text.html
-// @JorgeLuisBorges - Collected Fictions
 // @lmccart https://github.com/processing/p5.js/issues/536
+// @JorgeLuisBorges - Collected Fictions
 
+
+
+// var rs;
+// var counts;
+// var nouns = [];
 
 var go1;
 var go2;
@@ -13,10 +18,15 @@ var go3;
 var currentGram;
 var txt;
 var results = [];
+
+//markov chain works 4 characters at a time
+//this makes words readable without just copying text
 var order = 4;
+
 var ngrams = {};
 var beginnings = [];
 
+//loads text
 function preload() {
 	txt = loadStrings('borges.txt');
   console.log(txt);
@@ -24,6 +34,10 @@ function preload() {
 
 function setup() {
 	createCanvas(4000, 16000);
+
+//breaks down text into every possible set of 4 characters
+//draws 100 markov chains using characters
+//@DanielSchiffman
 	txt = join(txt, '\n');
 	for (go1 = 0; go1 < 100; go1++) {
 		for (var i = random(txt.length); i <= txt.length - order; i++) {
@@ -41,6 +55,8 @@ function setup() {
   }
 }
 
+//draws everything including styles
+//stops the loop so that characters stay in their position
 function draw() {
 	textSize(12);
 	textLeading(random(50));
@@ -50,12 +66,17 @@ function draw() {
 	noLoop();
 }
 
+
+//draws all of text to screen
+//each text is drawn with random kerning
 function display() {
 	for (var i = 0; i < results.length; i++) {
 	var x = 10;
 	for (go2 = 0; go2 < 10; go2++) {
 	var y = 0;
 	var incr =+ 1;
+
+	//@BryanMa
 		for (var k = 0; k < 100; k++) {
 			textAlign(CENTER,TOP);
 			var incrPos = k % incr;
@@ -64,11 +85,25 @@ function display() {
 			}
 			text(results[i].charAt(k), random(4000)+incrPos*(x-10), y);
 			x += random(1);
+
+			// rs = new RiString(results[i]);
+		  // rs.analyze();
+		  // rs.toLowerCase();
+			//
+		  // //getPosTags(rs);
+		  // console.log(rs.get(RiTa.POS));
+			//
+		  // console.log(rs.get(RiTa.POS));
+		  // console.log(rs.get(RiTa.STRESSES));
+		  // console.log(rs.get(RiTa.SYLLABLES));
+		  // console.log(rs.wordCount());
 		}
 	}
   }
 }
 
+//creates a markov chain of 200 characters
+//@DanielSchiffman
 function markovIt() {
 	currentGram = txt.substring(0, order);
 	results.push(currentGram);
@@ -82,11 +117,15 @@ function markovIt() {
 	console.log(results[results.length-1]);
 }
 
+//draws random character to screen
+//@tfguy44
 function mousePressed() {
   text("" + char(int(random(97,122))), mouseX, mouseY);
   return false;
 }
 
+//draws specified character to screen
+//@lmccart
 function keyTyped() {
 	fill(0);
 	text(key, mouseX, mouseY);
